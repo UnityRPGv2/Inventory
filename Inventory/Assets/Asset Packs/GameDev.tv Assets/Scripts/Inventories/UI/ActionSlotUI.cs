9c16f96e3b640a9837bb2a253266acf3a1452f84
Samuel Pattuzzi <sam@pattuzzi.it>
2019-12-04 14:16:56 +0000

    Comment out special actions bar for now.

--- Inventory/Assets/Asset Packs/GameDev.tv Assets/Scripts/Inventories/UI/ActionSlotUI.cs
+++ Inventory/Assets/Asset Packs/GameDev.tv Assets/Scripts/Inventories/UI/ActionSlotUI.cs
@@ -2,41 +2,41 @@
 using System.Collections.Generic;
 using RPG.Core.UI.Dragging;
 using RPG.Inventories;
-using RPG.SpecialActions;
+// using RPG.SpecialActions;
 using UnityEngine;
 
-namespace RPG.UI.Inventories
-{
-    public class ActionSlotUI : MonoBehaviour, IItemHolder, IDragContainer<InventoryItem>
-    {
-        [SerializeField] InventoryItemIcon _icon;
-        [SerializeField] int index = 0;
+// namespace RPG.UI.Inventories
+// {
+//     public class ActionSlotUI : MonoBehaviour, IItemHolder, IDragContainer<InventoryItem>
+//     {
+//         [SerializeField] InventoryItemIcon _icon;
+//         [SerializeField] int index = 0;
 
-        SpecialAbilities _abilitiesStore;
+//         SpecialAbilities _abilitiesStore;
 
-        public InventoryItem item { get => _abilitiesStore.GetAbility(index); }
+//         public InventoryItem item { get => _abilitiesStore.GetAbility(index); }
 
-        private void Awake() {
-            _abilitiesStore = GameObject.FindGameObjectWithTag("Player").GetComponent<SpecialAbilities>();
-            _abilitiesStore.OnAbilitiesUpdated += UpdateIcon;
-        }
+//         private void Awake() {
+//             _abilitiesStore = GameObject.FindGameObjectWithTag("Player").GetComponent<SpecialAbilities>();
+//             _abilitiesStore.OnAbilitiesUpdated += UpdateIcon;
+//         }
 
-        bool IDragContainer<InventoryItem>.CanAcceptItem(InventoryItem item)
-        {
-            return item is ActionConfig;
-        }
+//         bool IDragContainer<InventoryItem>.CanAcceptItem(InventoryItem item)
+//         {
+//             return item is ActionConfig;
+//         }
 
-        InventoryItem IDragContainer<InventoryItem>.ReplaceItem(InventoryItem item)
-        {
-            _abilitiesStore.SetAbility(item as ActionConfig, index);
+//         InventoryItem IDragContainer<InventoryItem>.ReplaceItem(InventoryItem item)
+//         {
+//             _abilitiesStore.SetAbility(item as ActionConfig, index);
 
-            // So that it isn't removed from previous place.
-            return item;
-        }
+//             // So that it isn't removed from previous place.
+//             return item;
+//         }
 
-        void UpdateIcon()
-        {
-            _icon.SetItem(item);
-        }
-    }
-}
+//         void UpdateIcon()
+//         {
+//             _icon.SetItem(item);
+//         }
+//     }
+// }
