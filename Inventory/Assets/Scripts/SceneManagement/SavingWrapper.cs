ab120a6f43956bef4c0dfa26991744bd1e478ed0
Samuel Pattuzzi <sam@pattuzzi.it>
2019-12-08 10:11:57 +0000

    Add in saving system.

---
+++ Inventory/Assets/Scripts/SceneManagement/SavingWrapper.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using GameDevTV.Saving;
+using UnityEngine;
+
+namespace RPG.SceneManagement
+{
+    public class SavingWrapper : MonoBehaviour
+    {
+        const string defaultSaveFile = "save";
+        
+        private void Awake() 
+        {
+            StartCoroutine(LoadLastScene());
+        }
+
+        private IEnumerator LoadLastScene() {
+            yield return GetComponent<SavingSystem>().LoadLastScene(defaultSaveFile);
+        }
+
+        private void Update() {
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                Save();
+            }
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                Load();
+            }
+            if (Input.GetKeyDown(KeyCode.Delete))
+            {
+                Delete();
+            }
+        }
+
+        public void Load()
+        {
+            GetComponent<SavingSystem>().Load(defaultSaveFile);
+        }
+
+        public void Save()
+        {
+            GetComponent<SavingSystem>().Save(defaultSaveFile);
+        }
+
+        public void Delete()
+        {
+            GetComponent<SavingSystem>().Delete(defaultSaveFile);
+        }
+    }
+}
