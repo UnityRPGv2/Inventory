2e0ddbd6b2ec485408e354693bb74471bd059e59
Samuel Pattuzzi <sam@pattuzzi.it>
2019-12-12 16:30:03 +0000

    Comment the saving system.

--- Inventory/Assets/Asset Packs/GameDev.tv Assets/Scripts/Saving/SaveableEntity.cs
+++ Inventory/Assets/Asset Packs/GameDev.tv Assets/Scripts/Saving/SaveableEntity.cs
@@ -7,10 +7,26 @@ using UnityEngine.AI;
 
 namespace GameDevTV.Saving
 {
+    /// <summary>
+    /// To be placed on any GameObject that has ISaveable components that
+    /// require saving.
+    ///
+    /// This class gives the GameObject a unique ID in the scene file. The ID is
+    /// used for saving and restoring the state related to this GameObject. This
+    /// ID can be manually override to link GameObjects between scenes (such as
+    /// recurring characters, the player or a score board). Take care not to set
+    /// this in a prefab unless you want to link all instances between scenes.
+    /// </summary>
     [ExecuteAlways]
     public class SaveableEntity : MonoBehaviour
     {
+        // CONFIG DATA
+        [Tooltip("The unique ID is automatically generated in a scene file if " +
+        "left empty. Do not set in a prefab unless you want all instances to " + 
+        "be linked.")]
         [SerializeField] string uniqueIdentifier = "";
+
+        // CACHED STATE
         static Dictionary<string, SaveableEntity> globalLookup = new Dictionary<string, SaveableEntity>();
 
         public string GetUniqueIdentifier()
@@ -18,6 +34,11 @@ namespace GameDevTV.Saving
             return uniqueIdentifier;
         }
 
+        /// <summary>
+        /// Will capture the state of all `ISaveables` on this component and
+        /// return a `System.Serializable` object that can restore this state
+        /// later.
+        /// </summary>
         public object CaptureState()
         {
             Dictionary<string, object> state = new Dictionary<string, object>();
@@ -28,6 +49,12 @@ namespace GameDevTV.Saving
             return state;
         }
 
+        /// <summary>
+        /// Will restore the state that was captured by `CaptureState`.
+        /// </summary>
+        /// <param name="state">
+        /// The same object that was returned by `CaptureState`.
+        /// </param>
         public void RestoreState(object state)
         {
             Dictionary<string, object> stateDict = (Dictionary<string, object>)state;
@@ -40,6 +67,8 @@ namespace GameDevTV.Saving
                 }
             }
         }
+
+        // PRIVATE
 
 #if UNITY_EDITOR
         private void Update() {
