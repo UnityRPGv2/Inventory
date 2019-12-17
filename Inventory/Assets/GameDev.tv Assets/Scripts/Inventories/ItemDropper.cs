29d3f2e6c2b98fd3922c670cb56ef1dca2fc5e12
Samuel Pattuzzi <sam@pattuzzi.it>
2019-12-17 09:49:19 +0000

    Dropping System

---
+++ Inventory/Assets/GameDev.tv Assets/Scripts/Inventories/ItemDropper.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using GameDevTV.Saving;
+
+namespace GameDevTV.Inventories
+{
+    /// <summary>
+    /// To be placed on anything that wishes to drop pickups into the world.
+    /// Tracks the drops for saving and restoring.
+    /// </summary>
+    public class ItemDropper : MonoBehaviour, ISaveable
+    {
+        // STATE
+        private List<Pickup> droppedItems = new List<Pickup>();
+
+        // PUBLIC
+
+        /// <summary>
+        /// Create a pickup at the current position.
+        /// </summary>
+        /// <param name="item">The item type for the pickup.</param>
+        public void DropItem(InventoryItem item)
+        {
+            SpawnPickup(item, GetDropLocation());
+        }
+
+        // PROTECTED
+
+        /// <summary>
+        /// Override to set a custom method for locating a drop.
+        /// </summary>
+        /// <returns>The location the drop should be spawned.</returns>
+        protected virtual Vector3 GetDropLocation()
+        {
+            return transform.position;
+        }
+
+        // PRIVATE
+
+        public void SpawnPickup(InventoryItem item, Vector3 spawnLocation)
+        {
+            var pickup = item.SpawnPickup(spawnLocation);
+            droppedItems.Add(pickup);
+        }
+
+        [System.Serializable]
+        private struct DropRecord
+        {
+            public string itemID;
+            public SerializableVector3 position;
+        }
+
+        object ISaveable.CaptureState()
+        {
+            RemoveDestroyedDrops();
+            var droppedItemsList = new DropRecord[droppedItems.Count];
+            for (int i = 0; i < droppedItemsList.Length; i++)
+            {
+                droppedItemsList[i].itemID = droppedItems[i].GetItem().GetItemID();
+                droppedItemsList[i].position = new SerializableVector3(droppedItems[i].transform.position);
+            }
+            return droppedItemsList;
+        }
+
+        void ISaveable.RestoreState(object state)
+        {
+            var droppedItemsList = (DropRecord[])state;
+            foreach (var item in droppedItemsList)
+            {
+                var pickupItem = InventoryItem.GetFromID(item.itemID);
+                Vector3 position = item.position.ToVector();
+                SpawnPickup(pickupItem, position);
+            }
+        }
+
+        /// <summary>
+        /// Remove any drops in the world that have subsequently been picked up.
+        /// </summary>
+        private void RemoveDestroyedDrops()
+        {
+            var newList = new List<Pickup>();
+            foreach (var item in droppedItems)
+            {
+                if (item != null)
+                {
+                    newList.Add(item);
+                }
+            }
+            droppedItems = newList;
+        }
+    }
+}
