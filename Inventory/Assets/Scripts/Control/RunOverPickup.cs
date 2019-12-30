e0cfb1334db94e407356bd70cb3ebfcf2e42cc48
Samuel Pattuzzi <sam@pattuzzi.it>
2019-12-30 10:11:59 +0000

    Clickable Pickups

---
+++ Inventory/Assets/Scripts/Control/RunOverPickup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GameDevTV.Inventories;
+using UnityEngine;
+
+namespace InventoryExample.Control
+{
+    [RequireComponent(typeof(Pickup))]
+    public class RunOverPickup : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other) 
+        {
+            var player = GameObject.FindGameObjectWithTag("Player");
+            if (other.gameObject == player)
+            {
+                GetComponent<Pickup>().PickupItem();
+            }
+        }
+    }
+}
