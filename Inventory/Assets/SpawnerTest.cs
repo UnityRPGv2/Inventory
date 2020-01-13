2ea7a1f1f7b4b0e652585eb70f6d3532e38f7c48
Samuel Pattuzzi <sam@pattuzzi.it>
2020-01-13 08:00:16 +0000

    Tooltip Prefab

---
+++ Inventory/Assets/SpawnerTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameDevTV.UI.Inventories;
+using GameDevTV.Inventories;
+
+public class SpawnerTest : MonoBehaviour
+{
+    [SerializeField] ItemTooltip tooltipPrefab = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ItemTooltip tooltipInstance = Instantiate(tooltipPrefab, transform);
+        tooltipInstance.Setup(InventoryItem.GetFromID("0aa7c8b8-4796-42aa-89d0-9d100ea67d7b"));
+    }
+
+}
